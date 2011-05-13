489bc3d7f74e7a2a28714d402eb712bb471bfdaf
emmanuel.c2011@gmail.com <emmanuel.c2011@gmail.com@36c3337a-611d-faa5-6aeb-0679fe8a3523>
2011-05-13 16:31:00 +0000

    Here's the COMPLETE updates!!! May need to clean-up comments.

--- trunk/Game/Game1.cs
+++ trunk/Game/Game1.cs
@@ -15,6 +15,9 @@ using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
 
+/*------------------------------EMMANUEL'S EDITS--------------------------------
+ * 01. Changed "LoadContent()" to work with new file format
+*/
 namespace guiCreator
 {
     /// <summary>
@@ -75,7 +78,7 @@ namespace guiCreator
                 string fileName = commandArgs[1];
                 if (File.Exists(fileName))
                 {
-                    currentLevel = new LinkedList<Sprite>();
+                    /*currentLevel = new LinkedList<Sprite>();
                     FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
                     StreamReader sr = new StreamReader(fs);
                     string type;
@@ -104,7 +107,99 @@ namespace guiCreator
                         Sprite a = (Sprite)Activator.CreateInstance(Type.GetType(type), args);
                         a.LoadContent(this.Content);
                         currentLevel.AddLast(a);
+                    }*/
+                    /*----------FUNC KEY---------
+                 * STM = Stream
+                 * ARG = Argument
+                 * ARY = Array
+                 * STR = string
+                 * LL  = LinkedList<>
+                 * SPR = Sprite
+                 * REP = Representate/Representation
+                 * DSPROBJ = Derived Sprite Object
+                             */
+
+                    // STMs for File & Reading. FileSTM opens the "fileName" file located here
+                    // "angelattack\guiCreator\bin\x86\Debug" And ReadSTM reads from that file.
+                    FileStream FS = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                    StreamReader SR = new StreamReader(FS);
+
+                    string SpriteTypeData = SR.ReadLine();      // Gets the SPROBJ for the ARGs
+                    // If SR.ReadLine() spits out "Cats love catfish" the below Split() is told to 
+                    // make FileLine[] = {"Cats", "love", "catfish"}. ReadLine() returns a STR
+                    string[] FileLine = SR.ReadLine().Split(new char[] { ' ' });
+                    object[] ArgumentValues;                    // Stores SPROBJ ARG values
+
+                    // Needed for loop control, ARY initialization & ARY iteration 
+                    int ArgumentLength = 0;
+                    int Iterator1 = 0;
+
+                    // "Type"(TY) variables are used to get "data about data" so info on the full name 
+                    // of a C# type or basically the location of where that its' defined can be gotten.
+                    // I don't know specifics but C# can somehow use the information specified in the TY
+                    // declaration to create instances of that type weather it be built-in or created by
+                    // a programmer. I guess it depends on the TY, number & order of ARGs to pick the
+                    // correct constructor for that TY. 
+                    Type ArgumentType;
+
+                    // When loading a file the level object is empty. Since 
+                    // or guiGame only works with LinkedLists(LL) & not strings from files we need the
+                    // appropriate objects to work with & these are it. 
+                    currentLevel = new LinkedList<Sprite>();
+
+                    // ALL RIGHT! Main loop!!! Below is exmaple of what's actually being read from file
+                    // guiCreator.Block
+                    // Argument = 2
+                    // System.Int32 680 320
+                    // Anyway Peek() looks at next char to be read but does not add char to STM. Instead
+                    // it returns a "int" val that REP of the next char to read(refer to ANSCII table)
+                    while (SR.Peek() != -1)
+                    {
+
+                        // Finding how many ARGs the SPR derived object needs from the STR vals in file.
+                        // Then allocating ARY to store them
+                        ArgumentLength = Convert.ToInt32(FileLine[3]);
+                        ArgumentValues = new object[ArgumentLength];
+
+                        // Reads & converts ARGs to proper types for use by derived SPR object(DSPROBJ)
+                        while (Iterator1 < ArgumentLength)
+                        {
+                            // The first line of ARGs. All values of same type. Refer above 4 "Split()"
+                            // FileLine[0] now has a STR REP of a type, which will be used later. 
+                            FileLine = SR.ReadLine().Split(new char[] { ' ' });
+                            ArgumentType = Type.GetType(FileLine[0]);
+
+                            // Iterates through each string element in "FileLine"
+                            foreach (string S in FileLine)
+                                if (S != FileLine[0])
+                                {
+                                    // This converts a STR REP of a value to a type IF it's possible.
+                                    // Won't work if attempted on value not of that type. 
+                                    ArgumentValues[Iterator1] = Convert.ChangeType(S, ArgumentType);
+
+                                    ++Iterator1;    // So next value is stored in a different element
+                                }
+                        }
+
+                        Sprite SpriteInstance = (Sprite)Activator.CreateInstance
+                                                (Type.GetType(SpriteTypeData), ArgumentValues);
+                        SpriteInstance.LoadContent(this.Content);
+
+                        // The Arguments are the last items read in a cycle of this loop. This makes 
+                        // sure "FileLine" can be stored with some data from the file
+                        if (SR.Peek() != -1)
+                        {
+                            SpriteTypeData = SR.ReadLine();
+                            FileLine = SR.ReadLine().Split(new char[] { ' ' });
+                        }
+
+                        Iterator1 = 0;  // Reset ARG iterator
+
+                        // Stores 1 piece of the level. 
+                        currentLevel.AddLast(SpriteInstance);
                     }
+                    SR.Close();
+                    FS.Close();
                 }
                 else
                 {
