eed7aef3b3c251354da7e3cc895111035c553a31
sarionel <sarionel@36c3337a-611d-faa5-6aeb-0679fe8a3523>
2011-06-16 23:31:42 +0000

    -changed sound function and added more hit effects

--- Game/Sound.cs
+++ Game/Sound.cs
@@ -22,10 +22,10 @@ namespace guiCreator
             //max of -1 means it only has one of its type and needs no random.
             switch (snd)
             {
-                case 1: type = "_attack";   max = 5; break;
-                case 2: type = "_hurt";     max = 5; break;
-                case 3: type = "_death";    max = 4; break;
-                case 4: type = "_special";  max = 4; break;
+                case 1: type = "_attack"; max = 5; break;
+                case 2: type = "_hurt"; max = 5; break;
+                case 3: type = "_death"; max = 4; break;
+                case 4: type = "_special"; max = 4; break;
                 default: type = null; max = -1; break;
             }
 
@@ -66,49 +66,51 @@ namespace guiCreator
         }   //end voice function
 
 
-
-        public void PlayEffect(ContentManager theContentManager, int effect)
+        //Overloaded two functions. This function includes an int
+        //in order to enable a random playing of a collection of sounds.
+        public void PlayEffect(ContentManager theContentManager, string effect, int numFiles)
         {
             directory = "audio//effect//";
 
-            //switches effect names. 
-            //max of -1 means it only has one of its type and needs no random.
-            switch (effect)
-            {
-                case 1: name = "walk_concrete"; max = 5; break;
-                case 2: name = "knife_swing"; max = 4; break;
-                case 3: name = "hitconnect"; max = 4; break;
-                default: name = "null"; max = -1; break;
-            }
+            //name of file
+            name = effect;
+            max = numFiles;
             SoundEffect soundFX;
 
-            //random number
-            if (max != -1)
-            {
-                Random rndsnd = new Random();
-                int rand = rndsnd.Next(1, max);
-                soundFX = theContentManager.Load<SoundEffect>(directory + name + rand);
-            }
-            else
-                soundFX = theContentManager.Load<SoundEffect>(directory + name);
+            Random rndsnd = new Random();
+            int rand = rndsnd.Next(1, max);
+            soundFX = theContentManager.Load<SoundEffect>(directory + name + rand);
 
-
-                
             sndCtrl = soundFX.Play();
+        }
 
 
-            if (sndCtrl != null)
+
+        //Overload with just string name,
+        //in case we want to play a single file with a name attached.
+        public void PlayEffect(ContentManager theContentManager, string effectName)
+        {
+            directory = "audio//effect//";
+
+            if (effectName != null)
             {
-                if (sndCtrl.State == SoundState.Stopped)
-                {
-                    sndCtrl.Dispose();
-                    sndCtrl = null;
-                }
+                name = effectName;
+                SoundEffect soundFX;
+                soundFX = theContentManager.Load<SoundEffect>(directory + name);
+                sndCtrl = soundFX.Play();
 
+
+                if (sndCtrl != null)
+                {
+                    if (sndCtrl.State == SoundState.Stopped)
+                    {
+                        sndCtrl.Dispose();
+                        sndCtrl = null;
+                    }
+                }
             }
 
         }   //end effect function
-
 
     }
 }
